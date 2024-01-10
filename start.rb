bcf6750e49dfe3214843e76e4ada7a496700a3ee
Sunny Hundal <amandip.h@gmail.com>
2024-01-09 23:36:26 -0800

    The first program + testing if ruby set up worked

---
+++ start.rb
@@ -0,0 +1,47 @@
+# The first program to make sure everything is working
+
+# We all start somewhere, so let's start with the basics of Ruby.
+
+# The first thing we need to do is print something to the screen.
+
+# puts is a method that prints something to the screen and then adds a new line.#Puts is short for "put string". the code below is a method and a string
+
+puts "Hello World!"
+
+# to run this program, type ruby start.rb in the terminal and hit enter. Make sure you are in the same directory as the file.
+
+# fun fact: We can also print emojis to the screen to make our programs more fun.
+puts "🫸🏻💥🫷🏻"
+
+# We can also print numbers to the screen.
+
+puts 1
+puts 2
+puts 3
+
+# We can also print the result of an expression to the screen.
+
+puts 1 + 1
+puts 2 * 2
+puts 3 / 3
+
+# We can also print the result of a method to the screen.
+
+puts "Hello World!".length
+puts "Hello World!".reverse
+puts "Hello World!".upcase
+
+# we can also use numbers to modify strings
+
+puts "Hello World! " * 2
+
+# we can also use variables to store values
+
+name = "Sunny"
+
+# the code above is called a variable. A variable is a container that stores a value. In this case, the variable name stores the value "Sunny".
+
+puts "Hello, my name is #{name}"
+
+# the #{} is called string interpolation. It allows us to insert the value of a variable into a string.
+
