5b7d063b13c2209473a0b36e82466f2934d65578
Sunny Hundal <amandip.h@gmail.com>
2024-01-30 23:57:12 -0800

    Added practice problems

---
+++ 06-exercises.rb
@@ -0,0 +1,18 @@
+# Exercise 1: Conditional Statements
+
+# Write a Ruby program that takes the user's age as input and prints whether they are eligible to vote or not.
+
+
+# Create a program that asks the user to input their exam grade and then outputs whether they passed or failed the exam.
+
+# Implement a Ruby script that determines if a given number is even or odd.
+# Write a program that checks if a person is eligible for a senior citizen discount based on their age (consider the age of 60 and above).
+
+
+# Exercise 2: Arrays and Hashes
+# Create an array of your favorite colors and print each color on a new line.
+
+# Build a hash representing a person with keys for name, age, and city. Print each key-value pair.
+# Write a program that reverses the elements in an array without using the reverse method.
+
+# Write a program that prints the elements in an array that are less than 5.
